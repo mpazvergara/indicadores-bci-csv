e379c95172ac7b6894f9e7440bf529c1a4966da1
Maria Paz <maria.paz.vergara@continuum.cl>
2018-01-11 16:58:25 -0300

    Add indicators as CSV

---
+++ indicadores.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+require 'bci'
+require 'csv'
+
+BCI = Bci::Client.new({ key: ENV['BCI_API_KEY'] })
+
+get '/indicadores.csv' do
+  kpi = BCI.stats.indicators['kpis']
+  if kpi.any?
+    CSV.generate do |csv|
+      csv << ["# Indicadores",""]
+      kpi.each do |kpi|
+        csv << [kpi['title'], kpi['price']]
+      end
+    end
+  end
+end
+
+get '/indicadores/*.csv' do |path|
+  kpi = BCI.stats.indicators['kpis']
+  if kpi.any?
+    CSV.generate do |csv|
+      kpi.each do |kpi|
+        if kpi['title'].downcase == path.downcase
+          csv << [kpi['price']]
+        else
+          "No existe el indicador"
+        end
+      end
+    end
+  end
+end
